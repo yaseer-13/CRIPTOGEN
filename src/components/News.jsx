b288c8a376682e01a0f53a2e9558420c56fa612e
Yaseer Tasleem <yaseertasleem@Local-Air.local>
2022-03-12 15:48:24 +0530

    Fixed Errors in News.JSX

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import moment from 'moment';
-import { useGetCryptosQuery } from '../services/cryptoApi';
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 import Loader from './Loader';
 
