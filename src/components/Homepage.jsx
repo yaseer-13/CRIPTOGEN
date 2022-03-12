f21e98084e913967529c43c093b42cfe9eb05145
Yaseer Tasleem <yaseertasleem@Local-Air.local>
2022-03-12 16:07:42 +0530

    added missing text

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -90,15 +90,13 @@ const Homepage = () => {
 
         <div className="home-heading-container mt-20 ">
           <h1 className="font-extrabold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-slate-700 to-pink-600 home-title">Top Cryptocurrencies</h1>
-          <a href="/cryptocurrencies" className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 ">
-          </a>
+          <a href="/cryptocurrencies" className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 ">Show More</a>
         </div>
         <Cryptocurrencies simplified />
 
         <div className="home-heading-container mt-20 ">
           <h1 className="font-extrabold  text-transparent text-2xl bg-clip-text bg-gradient-to-r from-slate-700 to-pink-600 home-title"> Cryptocurrencies News</h1>
-          <a href="/news" className="bg-pink-500 text-white hover:bg-slate-600 font-bold uppercase text-sm px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 ">
-          </a>
+          <a href="/news" className="bg-pink-500 text-white hover:bg-slate-600 font-bold uppercase text-sm px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 ">Show More</a>
         </div>
         <News simplified />
       </div >
